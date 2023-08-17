a837241588c8172c66a5a57225b74a3f0ab00971
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-08-17 10:54:30 +0100

    feat: google auth and db user creation

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,5 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "./prisma";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -11,4 +12,29 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (!profile?.email) {
+        throw new Error("No profile");
+      }
+      const user = await prisma.user.upsert({
+        where: {
+          email: profile.email,
+        },
+        create: {
+          email: profile.email,
+          name: profile.name,
+          image: (profile as any)?.picture,
+          org: {
+            create: {},
+          },
+        },
+        update: {
+          name: profile.name,
+          image: (profile as any)?.picture,
+        },
+      });
+      return true;
+    },
+  },
 };
